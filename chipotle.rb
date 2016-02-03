1ab19e3b0003a19486f8c7a401b96839b2cf5ec9
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-03 12:20:55 -0600

    Chipotle nested data structure exercise - bulk done

---
+++ chipotle.rb
@@ -0,0 +1,55 @@
+#Your at Chipotle where you are given 
+
+main = {
+	style: ["burrito", "bowl", "crispy corn tacos", "soft corn tacos", "soft flour tacos", "salad"],
+	meat: ["steak", "carnitas", "chicken", "barbacoa", "sofritas"],
+	rice_bean_veg: ["cilantro-lime brown rice", "cilantro-lime white rice", "black beans", "pinto beans", "fajita veggies"],
+	toppings: ["sour cream", "guacamole", "cheese", "romaine lettuce", "roasted chili-corn salsa", "tomatillo green-chilli salsa (medium hot)", "tomatillo red-chilli salsa (hot)", "fresh tomato salsa (mild)"]
+}
+
+#This line should not be changed, it is the starting hash for your order! 
+my_order = {}
+
+
+#INSTRUCTIONS:
+	#Use methods to add create your order, you can only call items from the main hash to them to the my_order hash- You should not be writing any strings unless you do the bonus. 
+
+	#By end it should look something like: 
+		#my_order = { style: "burrito", meat: "chicken", rice_bean_veg: ["cilantro-lime brown rice", "black beans"], toppings: ["guacamole", "cheese", "romaine lettuce"]}
+
+
+
+#STEP ONE:
+#you are in line at chipotle and what to know what different styles they have
+#Display all the options available for style 
+
+
+
+#STEP TWO:
+#Using the empty my_order hash, 
+#Chose your style from the available options and add a new key value pair to that hash
+
+
+#STEP THREE:
+#Chose what meat you want & add it to your my_order hash
+
+#STEP FOUR:
+#Chose what rice, bean or veggies you want and it it as a new key value pair to my_order. 
+	#HINT: You may want to chose more than one option, how will you store multiples in your order?
+
+
+#STEP FIVE
+#Chose at least 3 toppings to add to your order
+#Create a new key value pair
+
+
+
+####BONUS!#######
+	#Display your order in a sentence
+		#This will be challenging when it comes to working with your multiple choices for toppings
+
+	#Example: "Can I please have a burriot. For meat, I would like chicken with cilantro-lime white rice and black beans. Then I would like sour cream, cheese and roasted-corn salsa as my topping. Thank you!"
+
+
+
+	
