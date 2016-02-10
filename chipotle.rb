d71d3dfb090ddcb67aefde8e86d4006c5d0b8ce5
Tara Campbell <tac1998@gmail.com>
2016-02-10 13:34:10 -0600

    completed up to 7

--- chipotle.rb
+++ chipotle.rb
@@ -22,31 +22,39 @@ my_order = {}
 #STEP ONE:
 	#you are in line at chipotle and what to know what different styles they have
 	#Display all the options available for style 
-
+puts main[:style]
 
 
 #STEP TWO:
 	#Using the empty my_order hash, 
 	#Chose your style from the available options and add a new key value pair to that hash
-
+my_order[:style] = main[:style] [0]
 
 #STEP THREE:
 	#Chose what meat you want & add it to your my_order hash
+my_order[:meat] = main[:meat] [2]
 
 #STEP FOUR:
 	#Chose what rice, bean or veggies you want and it it as a new key value pair to my_order. 
 		#HINT: You may want to chose more than one option, how will you store multiples in your order?
+my_order[:rice_bean_veg] = main[:rice_bean_veg] [1,2]
+
+#or
+my_order[:toppings] = main[:toppings].select {|topping| topping == "romaine lettuce" || "guacamole"}
 
 #STEP FIVE
 	#Check to see if your favorite topping is included in the available options
-
+puts main.assoc(:toppings)
 
 #STEP SIX
 	#Chose at least 3 toppings to add to your order
-
+my_order[:toppings] = Array.new
+my_order[:toppings] << main[:toppings] [0]
+my_order[:toppings] << main[:toppings] [1]
+my_order[:toppings] << main[:toppings] [2]
 #STEP SEVEN
 	#Oops! You didn't want one of those toppings! How do you remove the second one you put in there?
-
+my_order[:toppings].delete ("guacamole") 
 
 #STEP EIGHT
 	#Print your hash and see if your order was made correctly! Bon Appetit! 
